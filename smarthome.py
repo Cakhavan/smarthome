1461954f91bf1cd220adcda945575f177faeda26
Cameron Akhavan <cameronakhavan@Camerons-MacBook-Pro-2.local>
2018-11-11 16:07:55 -0800

    commit

--- smarthome.py
+++ smarthome.py
@@ -52,50 +52,50 @@ def publish_callback(result, status):
  
 
 class MySubscribeCallback(SubscribeCallback):
-	def status(self, pubnub, status):
-    	pass
-		    # The status object returned is always related to subscribe but could contain
-		    # information about subscribe, heartbeat, or errors
-		    # use the operationType to switch on different options
-		    if status.operation == PNOperationType.PNSubscribeOperation \
-		            or status.operation == PNOperationType.PNUnsubscribeOperation:
-		        if status.category == PNStatusCategory.PNConnectedCategory:
-		            pass
-		            # This is expected for a subscribe, this means there is no error or issue whatsoever
-		        elif status.category == PNStatusCategory.PNReconnectedCategory:
-		            pass
-		            # This usually occurs if subscribe temporarily fails but reconnects. This means
-		            # there was an error but there is no longer any issue
-		        elif status.category == PNStatusCategory.PNDisconnectedCategory:
-		            pass
-		            # This is the expected category for an unsubscribe. This means there
-		            # was no error in unsubscribing from everything
-		        elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
-		            pass
-		            # This is usually an issue with the internet connection, this is an error, handle
-		            # appropriately retry will be called automatically
-		        elif status.category == PNStatusCategory.PNAccessDeniedCategory:
-		            pass
-		            # This means that PAM does allow this client to subscribe to this
-		            # channel and channel group configuration. This is another explicit error
-		        else:
-		            pass
-		            # This is usually an issue with the internet connection, this is an error, handle appropriately
-		            # retry will be called automatically
-		    elif status.operation == PNOperationType.PNSubscribeOperation:
-		        # Heartbeat operations can in fact have errors, so it is important to check first for an error.
-		        # For more information on how to configure heartbeat notifications through the status
-		        # PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
-		        if status.is_error():
-		            pass
-		            # There was an error with the heartbeat operation, handle here
-		        else:
-		            pass
-		            # Heartbeat operation was successful
-		    else:
-		        pass
-		        # Encountered unknown status type
-		 
+    def status(self, pubnub, status):
+        pass
+        # The status object returned is always related to subscribe but could contain
+        # information about subscribe, heartbeat, or errors
+        # use the operationType to switch on different options
+        if status.operation == PNOperationType.PNSubscribeOperation \
+                or status.operation == PNOperationType.PNUnsubscribeOperation:
+            if status.category == PNStatusCategory.PNConnectedCategory:
+                pass
+                # This is expected for a subscribe, this means there is no error or issue whatsoever
+            elif status.category == PNStatusCategory.PNReconnectedCategory:
+                pass
+                # This usually occurs if subscribe temporarily fails but reconnects. This means
+                # there was an error but there is no longer any issue
+            elif status.category == PNStatusCategory.PNDisconnectedCategory:
+                pass
+                # This is the expected category for an unsubscribe. This means there
+                # was no error in unsubscribing from everything
+            elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+                pass
+                # This is usually an issue with the internet connection, this is an error, handle
+                # appropriately retry will be called automatically
+            elif status.category == PNStatusCategory.PNAccessDeniedCategory:
+                pass
+                # This means that PAM does allow this client to subscribe to this
+                # channel and channel group configuration. This is another explicit error
+            else:
+                pass
+                # This is usually an issue with the internet connection, this is an error, handle appropriately
+                # retry will be called automatically
+        elif status.operation == PNOperationType.PNSubscribeOperation:
+            # Heartbeat operations can in fact have errors, so it is important to check first for an error.
+            # For more information on how to configure heartbeat notifications through the status
+            # PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
+            if status.is_error():
+                pass
+                # There was an error with the heartbeat operation, handle here
+            else:
+                pass
+                # Heartbeat operation was successful
+        else:
+            pass
+            # Encountered unknown status type
+
     def presence(self, pubnub, presence):
         pass  # handle incoming presence data
  
@@ -104,8 +104,8 @@ class MySubscribeCallback(SubscribeCallback):
         	lamp.on()
         	pubnub.publish().channel('ch1').message("lamp has been turned on").async(publish_callback)
         elif message.message == 'OFF':
- 			lamp.off()
- 			pubnub.publish().channel('ch1').message("lamp has been turned off").async(publish_callback)
+        	lamp.off()
+        	pubnub.publish().channel('ch1').message("lamp has been turned off").async(publish_callback)
 
 
 
@@ -120,23 +120,22 @@ pubnub.publish().channel('ch2').message([DHT_Read]).async(publish_callback)
 while True:
 
 
-	while(door_sensor.is_held)
+	while door_sensor.is_held():
 
-			doorCount++
-			door = "door has been opened: " + str(doorCount) + "times"
-
-			pubnub.publish().channel('ch1').message(door).async(publish_callback)
+		doorCount = doorCount + 1
+		door = "door has been opened: " + str(doorCount) + "times"
+		pubnub.publish().channel('ch1').message(door).async(publish_callback)
 
 
 	door = "door is closed"
 	pubnub.publish().channel('ch1').message(door).async(publish_callback)
 
 
-	while(light.is_held)
+	while light.is_held():
 		pubnub.publish().channel('ch1').message("lights are on").async(publish_callback)
 
 	pubnub.publish().channel('ch1').message("lights are off").async(publish_callback)
 
-    sleep(1)
+	sleep(1)
 
 
