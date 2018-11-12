69035537af6463bed2b2266690f7f70a6c77dbae
Cameron Akhavan <cameronakhavan@Camerons-MacBook-Pro-2.local>
2018-11-11 18:00:03 -0800

    dfj

--- smarthome.py
+++ smarthome.py
@@ -128,28 +128,31 @@ def publish_callback(result, status):
 
 
 while True:
-
-	if flag == 1:
-		lamp.on()
-	else:
-		lamp.off()
-
-	while door_sensor.is_held:
-
-		doorCount = doorCount + 1
-		door = "door has been opened: " + str(doorCount) + "times"
-		pubnub.publish().channel('ch1').message(door).async(publish_callback)
-
-
-	door = "door is closed"
-	pubnub.publish().channel('ch1').message(door).async(publish_callback)
-
-
-	while light.is_held:
-		pubnub.publish().channel('ch1').message("lights are on").async(publish_callback)
-
-	pubnub.publish().channel('ch1').message("lights are off").async(publish_callback)
-
+	lamp.on()
 	sleep(3)
+	lamp.off()
+	sleep(3)
+#	if flag == 1:
+#		lamp.on()
+#	else:
+#		lamp.off()
+
+#	while door_sensor.is_held:
+
+#		doorCount = doorCount + 1
+#		door = "door has been opened: " + str(doorCount) + "times"
+#		pubnub.publish().channel('ch1').message(door).async(publish_callback)
+
+
+#	door = "door is closed"
+#	pubnub.publish().channel('ch1').message(door).async(publish_callback)
+
+
+#	while light.is_held:
+#		pubnub.publish().channel('ch1').message("lights are on").async(publish_callback)
+
+#	pubnub.publish().channel('ch1').message("lights are off").async(publish_callback)
+#
+	
 
 
